a1abafaae4c716ac1323bb93ed58f5206481751c
DreamSmile <2925109633@QQ.com>
2021-04-28 11:02:35 +0800

    后台商家申请没有选中状态

--- src/router/index.js
+++ src/router/index.js
@@ -122,7 +122,7 @@ const routes = [
         component: merchantIndex,
       }, {
         path: '/applyMerchantIndex',
-        name: 'applyMerchantIndex',
+        name: 'ApplyMerchantIndex',
         component: applyMerchantIndex,
       }
       , {
